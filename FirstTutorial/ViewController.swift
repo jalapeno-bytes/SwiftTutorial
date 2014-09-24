5a703f09759fae6506302c14244483f8b9e48fbf
Han van Oostende <hvoostende@gmail.com>
2014-09-24 23:05:10 +0200

    Eerste Versie

--- FirstTutorial/ViewController.swift
+++ FirstTutorial/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField:UITextField!
+    @IBOutlet var webView:UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didClickGo(AnyObject) {
+        var text = textField.text
+        var url = NSURL.URLWithString(text)
+        var request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
     }
 
 
